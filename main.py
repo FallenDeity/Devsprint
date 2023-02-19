db76706908e46f23f1539095d4c0b0649500de26
FallenDeity <61227305+FallenDeity@users.noreply.github.com>
2023-02-20 00:28:00 +0530

    feat: refactor mount

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from src import Website
 
 app = Website(docs="/docs", redoc="/redocs")
 loop = asyncio.get_event_loop()
-loop.run_until_complete(app.on_startup())
+loop.create_task(app.on_startup())
 
 
 if __name__ == "__main__":
