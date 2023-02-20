aaed8d523de2f75c265550de87ad1cb3d558bdd1
FallenDeity <61227305+FallenDeity@users.noreply.github.com>
2023-02-21 02:33:17 +0530

    fix: dependency issue

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import asyncio
+# import asyncio
 
 from src import Website
 
 app = Website(docs="/docs", redoc="/redocs")
-loop = asyncio.get_event_loop()
-loop.create_task(app.on_startup())
+# loop = asyncio.get_event_loop()
+# loop.create_task(app.on_startup())
 
 
 if __name__ == "__main__":
